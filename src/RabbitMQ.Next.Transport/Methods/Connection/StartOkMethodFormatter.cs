ad45cdb64d4d33faea711afec357144f1133511d
sanych-sun <alexes.polyakov@gmail.com>
2020-10-05 00:36:54 -0700

    Remove debug code

--- src/RabbitMQ.Next.Transport/Methods/Connection/StartOkMethodFormatter.cs
+++ src/RabbitMQ.Next.Transport/Methods/Connection/StartOkMethodFormatter.cs
@@ -1,24 +1,14 @@
 using System;
-using System.IO;
 
 namespace RabbitMQ.Next.Transport.Methods.Connection
 {
     internal class StartOkMethodFormatter : IMethodFormatter<StartOkMethod>
     {
-        public Span<byte> Write(Span<byte> destination, StartOkMethod method)
-        {
-            var res = destination
+        public Span<byte> Write(Span<byte> destination, StartOkMethod method) =>
+            destination
                 .Write(method.ClientProperties)
                 .Write(method.Mechanism)
                 .Write(method.Response, true)
                 .Write(method.Locale);
-
-            using (var writer = new BinaryWriter(File.Open(@"/home/sanych-sun/Programming/RabbitMQ.Next/method.bin", FileMode.Create)))
-            {
-                writer.Write(destination.Slice(0, destination.Length - res.Length));
-            }
-
-            return res;
-        }
     }
 }
