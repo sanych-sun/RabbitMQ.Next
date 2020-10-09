6dec5e8b6f95fcc0d027138067163f323242b5f3
sanych-sun <alexes.polyakov@gmail.com>
2020-10-09 00:53:08 -0700

    Add ability to write bit flags

--- src/RabbitMQ.Next.Transport/BinaryWriteExtensions.cs
+++ src/RabbitMQ.Next.Transport/BinaryWriteExtensions.cs
@@ -276,5 +276,23 @@ namespace RabbitMQ.Next.Transport
 
             return target;
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<byte> WriteBits(
+            this Span<byte> target,
+            bool bit1, bool bit2 = false, bool bit3 = false, bool bit4 = false,
+            bool bit5 = false, bool bit6 = false, bool bit7 = false, bool bit8 = false)
+        {
+            var bits = Convert.ToByte(bit1);
+            bits |= (byte)(Convert.ToByte(bit2) << 1);
+            bits |= (byte)(Convert.ToByte(bit3) << 2);
+            bits |= (byte)(Convert.ToByte(bit4) << 3);
+            bits |= (byte)(Convert.ToByte(bit5) << 4);
+            bits |= (byte)(Convert.ToByte(bit6) << 5);
+            bits |= (byte)(Convert.ToByte(bit7) << 6);
+            bits |= (byte)(Convert.ToByte(bit8) << 7);
+
+            return target.Write(bits);
+        }
     }
 }
